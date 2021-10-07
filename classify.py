29efc07fc58a233b0a284f4e87f55cab25c37393
Pablousse <durieux.arnaud@hotmail.com>
2021-10-07 15:33:41 +0200

    refactor

--- classify.py
+++ classify.py
@@ -1,4 +1,35 @@
-from main import calc_closest_val
+import cv2
+from image_similarity_measures.quality_metrics import ssim
+import os
 
 
-calc_closest_val()
+def calc_closest_val(dict, checkMax):
+    result = {}
+    if (checkMax):
+        closest = max(dict.values())
+    else:
+        closest = min(dict.values())
+
+    for key, value in dict.items():
+        if (value == closest):
+            result[key] = closest
+
+    return result
+
+
+def classify_die(img_path: str):
+
+    data_dir = "data/normal_dice/classification"
+    similarity = {}
+
+    test_img = cv2.imread(img_path)
+
+    for file in os.listdir(data_dir):
+        classification_img_path = os.path.join(data_dir, file)
+        if img_path != classification_img_path:
+            data_img = cv2.imread(classification_img_path)
+            similarity[classification_img_path] = ssim(test_img, data_img)
+
+    similarity_max = calc_closest_val(similarity, True)
+
+    return os.path.split(list(similarity_max.keys())[0])[1].split(".")[0]
